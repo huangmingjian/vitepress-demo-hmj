87a1c69f20a134ac2d58b4643196af3cfc96f283
huangmingjian <huangmingjian@users.noreply.github.com>
2025-05-08 11:15:15 +0000

    deploy: 7df8a18e82696205186d98b08b0611a51d1fc608

---
+++ assets/linklocation_navigationbar.md.DKJ3W_7z.js
@@ -0,0 +1,50 @@
+import{_ as a,c as n,o as i,ae as e}from"./chunks/framework.CEJdHl6x.js";const l="/vitepress-demo-hmj/assets/title.D5YOy4ZJ.jpg",t="/vitepress-demo-hmj/assets/bar.I__SEGye.jpg",g=JSON.parse('{"title":"导航栏配置","description":"","frontmatter":{},"headers":[],"relativePath":"linklocation/navigationbar.md","filePath":"linklocation/navigationbar.md"}'),p={name:"linklocation/navigationbar.md"};function E(h,s,k,r,o,d){return i(),n("div",null,s[0]||(s[0]=[e('<h1 id="导航栏配置" tabindex="-1">导航栏配置 <a class="header-anchor" href="#导航栏配置" aria-label="Permalink to &quot;导航栏配置&quot;">​</a></h1><h2 id="_1-1、导航左侧" tabindex="-1">1.1、导航左侧 <a class="header-anchor" href="#_1-1、导航左侧" aria-label="Permalink to &quot;1.1、导航左侧&quot;">​</a></h2><p>需要配置 config.mts 文件下的title，如下代码所示: <img src="'+l+`" style="zoom:50%;"></p><h2 id="_1-2、右上角-导航内容自定义" tabindex="-1">1.2、右上角-导航内容自定义 <a class="header-anchor" href="#_1-2、右上角-导航内容自定义" aria-label="Permalink to &quot;1.2、右上角-导航内容自定义&quot;">​</a></h2><p>首先先确定nav在docs/.vitepress/config.ts文件的位置，具体如下:</p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">export default defineConfig({</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    themeConfig: {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        nav: [] // 这里传入一个数组，将相关的导航栏信息传递进来</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">})</span></span></code></pre></div><p>位置有了，接下来我们来定义navbar的内容。具体代码如下：</p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">// docs/.vitepress/relaConf/index.ts 配置内容较多，单独起个文件</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">export * from &#39;./navbar&#39;;</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">// docs/.vitepress/relaConf/navbar.ts</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">import { DefaultTheme } from &quot;vitepress&quot;;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">export const nav: DefaultTheme.NavItem[] = [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    { </span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">      text: &#39;首页&#39;,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">      link: &#39;/&#39;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        text:&#39;个人学习&#39;,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        items:[</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;cesium&#39;, link: &#39;../linklocation/learn-vitepress&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;@jodvf/cesium-3d&#39;, link: &#39;/frontend/&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;node.js&#39;, link: &#39;/frontend/&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;tailwindcss&#39;, link: &#39;/frontend/&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text:&#39;joplayer.js&#39;,link:&#39;/frontend/&#39;},</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;vitepress&#39;, link: &#39;../linklocation/learn-vitepress&#39; }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        ]</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    },{</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        text:&#39;公司学习&#39;,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        items:[</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;产品谱系&#39;, link: &#39;/frontend/&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;组织架构&#39;, link: &#39;/frontend/&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;业务领域&#39;, link: &#39;/frontend/&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;技术优势&#39;, link: &#39;/frontend/&#39; }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        ]</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">]</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">// 在config.mts中引用</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">import { defineConfig } from &#39;vitepress&#39;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">import { nav } from &#39;./relaConf&#39;</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">export default defineConfig({</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  title: &quot;huangmingjian_blog&quot;,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  description: &quot;unknow&quot;,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  themeConfig: {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    nav: nav,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    socialLinks: [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">      { icon: &#39;github&#39;, link: &#39;https://github.com/vuejs/vitepress&#39; }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    ]</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">})</span></span></code></pre></div><p>到这里，我们就完成了navbar的美化。具体来看下效果: <img src="`+t+'" style="zoom:50%;"></p><p>更多导航栏配置可以根据官方文档进行学习（<a href="https://vitepress.dev/zh/reference/default-theme-nav%EF%BC%89%E3%80%82" target="_blank" rel="noreferrer">https://vitepress.dev/zh/reference/default-theme-nav）。</a></p>',10)]))}const y=a(p,[["render",E]]);export{g as __pageData,y as default};
---
+++ assets/linklocation_sidebar.md.DxDCC_MA.js
@@ -0,0 +1,30 @@
+import{_ as a,c as i,o as n,ae as e}from"./chunks/framework.CEJdHl6x.js";const l="/vitepress-demo-hmj/assets/config1.yzFOimqt.jpg",t="/vitepress-demo-hmj/assets/config2.CpMk7AhA.jpg",g=JSON.parse('{"title":"导航栏配置","description":"","frontmatter":{},"headers":[],"relativePath":"linklocation/sidebar.md","filePath":"linklocation/sidebar.md"}'),p={name:"linklocation/sidebar.md"};function h(E,s,k,r,d,c){return n(),i("div",null,s[0]||(s[0]=[e(`<h1 id="导航栏配置" tabindex="-1">导航栏配置 <a class="header-anchor" href="#导航栏配置" aria-label="Permalink to &quot;导航栏配置&quot;">​</a></h1><h2 id="_1-1、默认配置" tabindex="-1">1.1、默认配置 <a class="header-anchor" href="#_1-1、默认配置" aria-label="Permalink to &quot;1.1、默认配置&quot;">​</a></h2><p>需要配置 config.mts 文件下的themeConfig.sidebar，如下代码所示:</p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">sidebar: [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">   {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">     text: &#39;Examples&#39;,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">     items: [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">       { text: &#39;Markdown Examples&#39;, link: &#39;/markdown-examples&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">       { text: &#39;Runtime API Examples&#39;, link: &#39;/api-examples&#39; }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">     ]</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">   }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">]</span></span></code></pre></div><p>配置结果: <img src="`+l+`" style="zoom:50%;"></p><h2 id="_1-2、多侧边栏" tabindex="-1">1.2、多侧边栏 <a class="header-anchor" href="#_1-2、多侧边栏" aria-label="Permalink to &quot;1.2、多侧边栏&quot;">​</a></h2><p>会根据页面路径显示不同的侧边栏。</p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">sidebar: {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    &#39;/linklocation/&#39;: [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        text: &#39;vitepress&#39;,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        items: [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;项目创建&#39;, link: &#39;/linklocation/learn-vitepress&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;导航栏配置&#39;, link: &#39;/linklocation/navigationbar&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;侧边栏配置&#39;, link: &#39;/linklocation/sidebar&#39; }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        ]</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    ],</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    &#39;/config/&#39;: [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        text: &#39;Examples&#39;,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        items: [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;Index&#39;, link: &#39;/config/&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;Three&#39;, link: &#39;/config/three&#39; },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">            { text: &#39;Four&#39;, link: &#39;/config/four&#39; }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        ]</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    ]</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span></code></pre></div><p>linklocation为路径前缀,当路由为linklocation开头时，显示对应的侧边栏。 配置结果: <img src="`+t+'" style="zoom:50%;"></p><p>其他更多配置可以参考官方文档（<a href="https://vitepress.dev/zh/reference/default-theme-sidebar%EF%BC%89" target="_blank" rel="noreferrer">https://vitepress.dev/zh/reference/default-theme-sidebar）</a></p>',10)]))}const m=a(p,[["render",h]]);export{g as __pageData,m as default};
